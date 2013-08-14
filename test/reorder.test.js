c998ae81aecdcc4a5b81f146eadfa937ed83d13e
Troy Whiteley <troy@somanyscientists.com>
2013-08-14 15:20:38 -0700

    Somewhat working example.

--- test/reorder.test.js
+++ test/reorder.test.js
@@ -1,4 +1,20 @@
 
 suite('reorder', function() {
+  var list;
+  suiteSetup(function(){
+    list = $('#reorderable');
+  });
+
+  suite('general', function() {
+    test('jQuery element should have reorder function',function(){
+      list.reorder();
+      assert.equal(typeof list.reorder, "function");
+    });
+
+    test('should return list of items', function() {
+      list.reorder();
+      assert.equal(list.find('[draggable]').length, list.reorder('getItems').length);
+    });
+  });
 
 });
