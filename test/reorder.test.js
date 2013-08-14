d65261f6807392bb64b56009980b0611908311cd
Troy Whiteley <troy@somanyscientists.com>
2013-08-14 16:45:04 -0700

    Added tests!

--- test/reorder.test.js
+++ test/reorder.test.js
@@ -1,4 +1,24 @@
 
+function simulateDrag(el, dropIndex) {
+  el.reorder('onDrag', {
+    target: el.find('li').first(),
+    originalEvent: {
+      dataTransfer: {
+        setData: function() {},
+        effectAllowed: ''
+      }
+    }
+  });
+
+  el.reorder('onDragOver', {
+    target: el.find('li').eq(dropIndex - 1)
+  });
+
+  el.reorder('onDragDrop', {});
+
+  el.reorder('onDragEnd', {});
+}
+
 suite('reorder', function() {
   var list;
   suiteSetup(function(){
@@ -17,4 +37,71 @@ suite('reorder', function() {
     });
   });
 
+  suite('dragging', function() {
+    test('element shouldn\'t move if position the same', function(){
+      list.reorder();
+
+      var listHTML = list.html();
+      
+      simulateDrag(list, 1);
+
+      assert.equal(listHTML, list.html());
+    });
+
+    test('list should update when item dragged', function(){
+      list.reorder();
+
+      var listHTML = list.html();
+      
+      simulateDrag(list, 4);
+
+      assert.notEqual(listHTML, list.html());
+    });
+  });
+
+  suite('events', function(){
+    test('dragstart', function(done){
+      list.reorder();
+
+      list.one('dragstart', function(){
+        assert.ok(1);
+        done();
+      });
+
+      simulateDrag(list, 1);
+    });
+
+    test('dragover', function(done){
+      list.reorder();
+
+      list.one('dragover', function(){
+        assert.ok(1);
+        done();
+      });
+
+      simulateDrag(list, 4);
+    });
+
+    test('dragdrop', function(done){
+      list.reorder();
+
+      list.one('dragdrop', function(){
+        assert.ok(1);
+        done();
+      });
+
+      simulateDrag(list, 4);
+    });
+
+    test('dragend', function(done){
+      list.reorder();
+
+      list.one('dragend', function(){
+        assert.ok(1);
+        done();
+      });
+
+      simulateDrag(list, 4);
+    });
+  });
 });
